459ccec351ece6f1fb7f65bb8ccf3b087c088eae
Diego Salas <diesalasmart@gmail.com>
2021-12-06 12:45:59 -0500

    add doctor reducer to root reducer

--- src/services/redux/reducers/rootReducer.js
+++ src/services/redux/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
 import changeState from './changeState'
+import doctor from './doctorReducer'
 
 const rootReducer = combineReducers({
 	changeState,
+	doctor
 })
 
 export default rootReducer
