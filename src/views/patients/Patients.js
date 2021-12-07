96aa5d21bbc8b924f89bb8890091cc7ba7b9911b
Diego Salas <diesalasmart@gmail.com>
2021-12-07 01:11:44 -0500

    add patients file

---
+++ src/views/patients/Patients.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Patients extends Component {
+    render() {
+        return (
+            <div>
+                TO IMPLEMENT
+            </div>
+        )
+    }
+}
