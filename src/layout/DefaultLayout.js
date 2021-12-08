bba99cf929e29da9e3ca3572a430bcd8d922ac42
Diego Salas <diesalasmart@gmail.com>
2021-12-08 00:15:15 -0500

    fetch specialties on default layout

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as specialtyActions from '../services/redux/actions/specialtyActions'
 
-const DefaultLayout = () => {
-  return (
-    <div>
-      <AppSidebar />
-      <div className="wrapper d-flex flex-column min-vh-100 bg-light">
-        <AppHeader />
-        <div className="body flex-grow-1 px-3">
-          <AppContent />
+export class DefaultLayout extends Component {
+  async componentDidMount(){
+    await this.props.getSpecialties();
+  }
+
+  render() {
+    return (
+      <div>
+        <AppSidebar />
+        <div className="wrapper d-flex flex-column min-vh-100 bg-light">
+          <AppHeader />
+          <div className="body flex-grow-1 px-3">
+            <AppContent />
+          </div>
+          <AppFooter />
         </div>
-        <AppFooter />
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default DefaultLayout
+const mapStateToProps = state => {
+  return {
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+      ...bindActionCreators(Object.assign({}, specialtyActions), dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DefaultLayout)
