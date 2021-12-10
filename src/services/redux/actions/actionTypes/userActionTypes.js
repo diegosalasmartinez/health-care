77e0d81a6aa236c85a0625a1dcbf3938e2ab9e99
Diego Salas <diesalasmart@gmail.com>
2021-12-09 22:08:43 -0500

    create user redux files

---
+++ src/services/redux/actions/actionTypes/userActionTypes.js
@@ -0,0 +1,5 @@
+export const GET_USERS = "GET_USERS"
+export const CREATE_USER = "CREATE_USER"
+export const UPDATE_USER = "UPDATE_USER"
+export const DELETE_USER = "DELETE_USER"
+export const ERROR_USER = "ERROR_USER"
