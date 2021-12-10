c6e67d5bacb3c9b296b7a46663d579794a3c9134
Diego Salas <diesalasmart@gmail.com>
2021-12-09 22:39:58 -0500

    add users route

--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,15 @@ const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const Specialties = React.lazy(() => import('./views/specialties/Specialties'))
 const Doctors = React.lazy(() => import('./views/doctors/Doctors'))
 const Patients = React.lazy(() => import('./views/patients/Patients'))
+const Users = React.lazy(() => import('./views/users/Users'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/specialties', name: 'Specialties', component: Specialties },
   { path: '/doctors', name: 'Doctors', component: Doctors },
-  { path: '/patients', name: 'Patients', component: Patients }
+  { path: '/patients', name: 'Patients', component: Patients },
+  { path: '/users', name: 'Users', component: Users }
 ]
 
 export default routes
