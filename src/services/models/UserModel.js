d10ce6010ff77e7419df114a29baf8b258cadb82
Diego Salas <diesalasmart@gmail.com>
2021-12-22 19:05:39 -0500

    user validations

--- src/services/models/UserModel.js
+++ src/services/models/UserModel.js
@@ -5,9 +5,47 @@ export default class UserModel extends SimpleProperty {
     _id = "";
     personId = "";
     doctorId = "";
-    role = "";
+    role = "ADMIN";
     active = true;
     username = "";
     password = "";
     personInfo = new PersonModel();
 }
+
+const validate = (user, validateCredentials) => {
+    let errors = {
+        DNI: null,
+        name: null,
+        lastName: null,
+        email: null,
+        phone: null,
+        username: null,
+        password: null
+    }
+    if (!user.personInfo.DNI) {
+        errors.DNI = "DNI is mandatory";
+    }
+    if (!user.personInfo.name) {
+        errors.name = "Name is mandatory";
+    }
+    if (!user.personInfo.lastName) {
+        errors.lastName = "Last name is mandatory";
+    }
+    if (!user.personInfo.email) {
+        errors.email = "Email is mandatory";
+    }
+    if (!user.personInfo.phone) {
+        errors.phone = "Phone is mandatory";
+    }
+    if (validateCredentials) {
+        if (!user.username) {
+            errors.username = "Username is mandatory";
+        }
+        if (!user.password) {
+            errors.password = "Password is mandatory";
+        }
+    }
+    return errors;
+}
+
+export { validate }
--- src/views/users/Users.js
+++ src/views/users/Users.js
@@ -63,8 +63,12 @@ export class Users extends Component {
         this.setState({showConfirmationModal: true, mode: this.state.mode, userSelected: {...user}});
     }
 
-    onClose = () => {
-        this.setState({showOffcanvas: false, showConfirmationModal:false, mode: actionTypes.NONE, userSelected: new UserModel()});
+    onCloseOffcanvas = () => {
+        this.setState({showOffcanvas: false, mode: actionTypes.NONE, userSelected: new UserModel()});
+    }
+
+    onCloseConfirmation = () => {
+        this.setState({showConfirmationModal:false});
     }
 
     onSave = async (user) => {
@@ -129,7 +133,7 @@ export class Users extends Component {
                             mode={mode} 
                             userSelected={userSelected}
                             onSave={this.onAccept}
-                            onClose={this.onClose}
+                            onClose={this.onCloseOffcanvas}
                         />
                         <Confirmation
                             type="user"
@@ -138,7 +142,7 @@ export class Users extends Component {
                             object={userSelected}
                             visible={showConfirmationModal} 
                             onAccept={this.onSave}
-                            onClose={this.onClose}
+                            onClose={this.onCloseConfirmation}
                         />
                     </CRow>
                 }
