88109f3f4ba06fd0db3143b7da10e91b53194d4c
Diego Salas <diesalasmart@gmail.com>
2021-12-29 18:55:02 -0500

    limit access route by role

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ const Patients = React.lazy(() => import('./views/patients/Patients'))
 const PatientDetails = React.lazy(() => import('./views/patients/PatientDetails'))
 const Users = React.lazy(() => import('./views/users/Users'))
 
-const routes = [
+const allRoutes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/specialties', name: 'Specialties', component: Specialties },
@@ -18,4 +18,35 @@ const routes = [
   { path: '/users', name: 'Users', component: Users }
 ]
 
-export default routes
+const routesAdmin = [
+  { path: '/', exact: true, name: 'Home' },
+  { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+  { path: '/specialties', name: 'Specialties', component: Specialties },
+  { path: '/doctors', name: 'Doctors', component: Doctors },
+  { path: '/patients', name: 'Patients', component: Patients, exact: true },
+  { path: '/patients/create', name: 'Patient Details', component: PatientDetails },
+  { path: '/users', name: 'Users', component: Users }
+]
+
+const routesDoctor = [
+  { path: '/', exact: true, name: 'Home' },
+  { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+  { path: '/patients', name: 'Patients', component: Patients, exact: true },
+  { path: '/patients/create', name: 'Patient Details', component: PatientDetails },
+]
+
+const routesSecretary = [
+  { path: '/', exact: true, name: 'Home' },
+  { path: '/specialties', name: 'Specialties', component: Specialties },
+  { path: '/doctors', name: 'Doctors', component: Doctors },
+  { path: '/patients', name: 'Patients', component: Patients, exact: true },
+  { path: '/patients/create', name: 'Patient Details', component: PatientDetails },
+  { path: '/users', name: 'Users', component: Users }
+]
+
+export {
+  allRoutes,
+  routesAdmin,
+  routesDoctor,
+  routesSecretary
+}
