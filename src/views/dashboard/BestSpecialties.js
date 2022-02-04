f5b5d7cf929c2b85d7eee23c8935701f27b3aef6
Diego Salas <diesalasmart@gmail.com>
2022-02-03 23:37:34 -0500

    refactor best specialties componente

--- src/views/dashboard/BestSpecialties.js
+++ src/views/dashboard/BestSpecialties.js
@@ -2,34 +2,58 @@ import React, { Component } from 'react';
 import {
     CRow,
     CCol,
-    CDropdown,
-    CDropdownMenu,
-    CDropdownItem,
-    CDropdownToggle,
-    CWidgetStatsA,
+    CCard,
+    CCardBody,
+    CCardHeader,
 } from '@coreui/react'
-import { getStyle } from '@coreui/utils'
-import { CChartBar, CChartLine } from '@coreui/react-chartjs'
-import CIcon from '@coreui/icons-react'
-import { cilArrowBottom, cilArrowTop, cilOptions } from '@coreui/icons'
 
 export default class BestSpecialties extends Component {
-  render() {
-    return (
-        <CRow>
-            <CCol xs="4">
-                <CWidgetStatsA
-                    className="mb-4"
-                    color="primary"
-                    value={
-                    <>
-                        26K{' '}
-                    </>
-                    }
-                    title="Users"
-                />
-            </CCol>
-        </CRow>
-    )
-  }
+    getColor = (index) => {
+        switch(index){
+            case 0:
+                return "border-start-info";
+            case 1:
+                return "border-start-warning";
+            case 2:
+                return "border-start-light";
+            default:
+                return "border-start-info";
+        }
+    }
+
+    render() {
+        const { bestSpecialties = [] } = this.props;
+        return (
+            <CCard>
+                <CCardHeader>
+                Best Specialties
+                </CCardHeader>
+                <CCardBody>
+                    {/* <CRow className='mb-3'>
+                        <CCol>
+                            <h4 id="traffic" className="card-title mb-0">
+                                Best Specialties
+                            </h4>
+                        </CCol>
+                    </CRow> */}
+                    <CRow>
+                        { bestSpecialties.map((s, index) => 
+                            <CCol xs="4" key={index}>
+                                <div className={"border-start border-start-4 py-1 px-3 " + this.getColor(index)}>
+                                    <div className="text-medium-emphasis small">{"# Appointments: " + s.count}</div>
+                                    <div className="fs-5 fw-semibold">{s.specialty.name}</div>
+                                </div>
+                                {/* <CWidgetStatsA
+                                    className="mb-4 pb-2"
+                                    color="primary"
+                                    value={<>{s.specialty.name + ' '}</>}
+                                    title={"# Appointments: " + s.count}
+                                /> */}
+                            </CCol>
+                        )}
+                    </CRow>
+                </CCardBody>
+            </CCard>
+        )
+    }
 }
