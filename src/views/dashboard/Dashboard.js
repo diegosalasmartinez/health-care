516906ce15fec3ceafeaeb241ddce6b3ea0a4be0
Diego Salas <diesalasmart@gmail.com>
2022-02-05 19:53:37 -0500

    include best specialties and appointment chart

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -13,6 +13,7 @@ import {
   CCol,
   CProgress,
   CRow,
+  CSpinner,
   CTable,
   CTableBody,
   CTableDataCell,
@@ -52,8 +53,10 @@ import avatar3 from 'src/assets/images/avatars/3.jpg'
 import avatar4 from 'src/assets/images/avatars/4.jpg'
 import avatar5 from 'src/assets/images/avatars/5.jpg'
 import avatar6 from 'src/assets/images/avatars/6.jpg'
+import colorTypes from 'src/services/models/others/colorTypes'
 
 const BestSpecialties = lazy(() => import('./BestSpecialties'))
+const AppointmentHistory = lazy(() => import('./AppointmentHistory'))
 const WidgetsDropdown = lazy(() => import('../widgets/WidgetsDropdown.js'))
 const WidgetsBrand = lazy(() => import('../widgets/WidgetsBrand.js'))
 
@@ -184,6 +187,7 @@ export class Dashboard extends Component {
     super(props);
     this.state = {
         bestSpecialties: [],
+        history: [],
         notifications: [],
         loaded: false,
         failed: false,
@@ -194,7 +198,8 @@ export class Dashboard extends Component {
   async componentDidMount() {
     this.setState({loaded: false, failed: false});
     const bestSpecialties = await this.props.getBestSpecialties();
-    this.setState({bestSpecialties, loaded: true, failed: false});
+    const history = await this.props.getDashboardHistory();
+    this.setState({bestSpecialties, history, loaded: true, failed: false});
   }
  
   random = (min, max) => {
@@ -202,139 +207,150 @@ export class Dashboard extends Component {
   }
 
   render() {
-    const { loaded, failed, notifications, bestSpecialties } = this.state;
+    const { loaded, failed, notifications, bestSpecialties, history } = this.state;
 
     return (
       <>
-        <BestSpecialties bestSpecialties={bestSpecialties}/>
-        {/* <WidgetsDropdown /> */}
-        {/* <CCard className="mb-4">
-          <CCardBody>
-            <CRow>
-              <CCol sm={5}>
-                <h4 id="traffic" className="card-title mb-0">
-                  Traffic
-                </h4>
-                <div className="small text-medium-emphasis">January - July 2021</div>
-              </CCol>
-              <CCol sm={7} className="d-none d-md-block">
-                <CButton color="primary" className="float-end">
-                  <CIcon icon={cilCloudDownload} />
-                </CButton>
-                <CButtonGroup className="float-end me-3">
-                  {['Day', 'Month', 'Year'].map((value) => (
-                    <CButton
-                      color="outline-secondary"
-                      key={value}
-                      className="mx-0"
-                      active={value === 'Month'}
-                    >
-                      {value}
+        { loaded && !failed && 
+          <>
+            <BestSpecialties bestSpecialties={bestSpecialties}/>
+            <AppointmentHistory history={history}/>
+            {/* <WidgetsDropdown /> */}
+            <CCard className="mb-4">
+              <CCardBody>
+                <CRow>
+                  <CCol sm={5}>
+                    <h4 id="traffic" className="card-title mb-0">
+                      Traffic
+                    </h4>
+                    <div className="small text-medium-emphasis">January - July 2021</div>
+                  </CCol>
+                  <CCol sm={7} className="d-none d-md-block">
+                    <CButton color="primary" className="float-end">
+                      <CIcon icon={cilCloudDownload} />
                     </CButton>
+                    <CButtonGroup className="float-end me-3">
+                      {['Day', 'Month', 'Year'].map((value) => (
+                        <CButton
+                          color="outline-secondary"
+                          key={value}
+                          className="mx-0"
+                          active={value === 'Month'}
+                        >
+                          {value}
+                        </CButton>
+                      ))}
+                    </CButtonGroup>
+                  </CCol>
+                </CRow>
+                <CChartLine
+                  style={{ height: '300px', marginTop: '40px' }}
+                  data={{
+                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                    datasets: [
+                      {
+                        label: 'My First dataset',
+                        backgroundColor: hexToRgba(getStyle('--cui-info'), 10),
+                        borderColor: getStyle('--cui-info'),
+                        pointHoverBackgroundColor: getStyle('--cui-info'),
+                        borderWidth: 2,
+                        data: [
+                          this.random(50, 200),
+                          this.random(50, 200),
+                          this.random(50, 200),
+                          this.random(50, 200),
+                          this.random(50, 200),
+                          this.random(50, 200),
+                          this.random(50, 200),
+                        ],
+                        fill: true,
+                      },
+                      {
+                        label: 'My Second dataset',
+                        backgroundColor: 'transparent',
+                        borderColor: getStyle('--cui-success'),
+                        pointHoverBackgroundColor: getStyle('--cui-success'),
+                        borderWidth: 2,
+                        data: [
+                          this.random(50, 200),
+                          this.random(50, 200),
+                          this.random(50, 200),
+                          this.random(50, 200),
+                          this.random(50, 200),
+                          this.random(50, 200),
+                          this.random(50, 200),
+                        ],
+                      },
+                      {
+                        label: 'My Third dataset',
+                        backgroundColor: 'transparent',
+                        borderColor: getStyle('--cui-danger'),
+                        pointHoverBackgroundColor: getStyle('--cui-danger'),
+                        borderWidth: 1,
+                        borderDash: [8, 5],
+                        data: [65, 65, 65, 65, 65, 65, 65],
+                      },
+                    ],
+                  }}
+                  options={{
+                    maintainAspectRatio: false,
+                    plugins: {
+                      legend: {
+                        display: false,
+                      },
+                    },
+                    scales: {
+                      x: {
+                        grid: {
+                          drawOnChartArea: false,
+                        },
+                      },
+                      y: {
+                        ticks: {
+                          beginAtZero: true,
+                          maxTicksLimit: 5,
+                          stepSize: Math.ceil(250 / 5),
+                          max: 250,
+                        },
+                      },
+                    },
+                    elements: {
+                      line: {
+                        tension: 0.4,
+                      },
+                      point: {
+                        radius: 0,
+                        hitRadius: 10,
+                        hoverRadius: 4,
+                        hoverBorderWidth: 3,
+                      },
+                    },
+                  }}
+                />
+              </CCardBody>
+              <CCardFooter>
+                <CRow xs={{ cols: 1 }} md={{ cols: 5 }} className="text-center">
+                  {progressExample.map((item, index) => (
+                    <CCol className="mb-sm-2 mb-0" key={index}>
+                      <div className="text-medium-emphasis">{item.title}</div>
+                      <strong>
+                        {item.value} ({item.percent}%)
+                      </strong>
+                      <CProgress thin className="mt-2" color={item.color} value={item.percent} />
+                    </CCol>
                   ))}
-                </CButtonGroup>
-              </CCol>
-            </CRow>
-            <CChartLine
-              style={{ height: '300px', marginTop: '40px' }}
-              data={{
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                datasets: [
-                  {
-                    label: 'My First dataset',
-                    backgroundColor: hexToRgba(getStyle('--cui-info'), 10),
-                    borderColor: getStyle('--cui-info'),
-                    pointHoverBackgroundColor: getStyle('--cui-info'),
-                    borderWidth: 2,
-                    data: [
-                      this.random(50, 200),
-                      this.random(50, 200),
-                      this.random(50, 200),
-                      this.random(50, 200),
-                      this.random(50, 200),
-                      this.random(50, 200),
-                      this.random(50, 200),
-                    ],
-                    fill: true,
-                  },
-                  {
-                    label: 'My Second dataset',
-                    backgroundColor: 'transparent',
-                    borderColor: getStyle('--cui-success'),
-                    pointHoverBackgroundColor: getStyle('--cui-success'),
-                    borderWidth: 2,
-                    data: [
-                      this.random(50, 200),
-                      this.random(50, 200),
-                      this.random(50, 200),
-                      this.random(50, 200),
-                      this.random(50, 200),
-                      this.random(50, 200),
-                      this.random(50, 200),
-                    ],
-                  },
-                  {
-                    label: 'My Third dataset',
-                    backgroundColor: 'transparent',
-                    borderColor: getStyle('--cui-danger'),
-                    pointHoverBackgroundColor: getStyle('--cui-danger'),
-                    borderWidth: 1,
-                    borderDash: [8, 5],
-                    data: [65, 65, 65, 65, 65, 65, 65],
-                  },
-                ],
-              }}
-              options={{
-                maintainAspectRatio: false,
-                plugins: {
-                  legend: {
-                    display: false,
-                  },
-                },
-                scales: {
-                  x: {
-                    grid: {
-                      drawOnChartArea: false,
-                    },
-                  },
-                  y: {
-                    ticks: {
-                      beginAtZero: true,
-                      maxTicksLimit: 5,
-                      stepSize: Math.ceil(250 / 5),
-                      max: 250,
-                    },
-                  },
-                },
-                elements: {
-                  line: {
-                    tension: 0.4,
-                  },
-                  point: {
-                    radius: 0,
-                    hitRadius: 10,
-                    hoverRadius: 4,
-                    hoverBorderWidth: 3,
-                  },
-                },
-              }}
-            />
-          </CCardBody>
-          <CCardFooter>
-            <CRow xs={{ cols: 1 }} md={{ cols: 5 }} className="text-center">
-              {progressExample.map((item, index) => (
-                <CCol className="mb-sm-2 mb-0" key={index}>
-                  <div className="text-medium-emphasis">{item.title}</div>
-                  <strong>
-                    {item.value} ({item.percent}%)
-                  </strong>
-                  <CProgress thin className="mt-2" color={item.color} value={item.percent} />
-                </CCol>
-              ))}
-            </CRow>
-          </CCardFooter>
-        </CCard> */}
-  
+                </CRow>
+              </CCardFooter>
+            </CCard>
+          </>
+        }
+
+        { !loaded &&
+          <CRow className="center">
+            <CSpinner color={colorTypes.PRIMARY}/>
+          </CRow>
+        }
+
         {/* <WidgetsBrand withCharts /> */}
   
         {/* <CRow>
